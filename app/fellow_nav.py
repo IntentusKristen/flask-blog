f34f3cc29b5d842b8ba75be1eb93dfab4964fe89
IntentusKristen <85136882+IntentusKristen@users.noreply.github.com>
2022-06-02 15:25:56 -0400

    rearranged photo and name

--- app/fellow_nav.py
+++ app/fellow_nav.py
@@ -7,7 +7,7 @@ fellow_nav = Blueprint('fellow_nav', __name__)
 
 @fellow_nav.route('/kristen')
 def kristen():
-    return render_template('kristen_page.html', title="Kristen Zhang", url=os.getenv("URL"))
+    return render_template('kristen_page.html', title="22.SUM.22 Fellow: Kristen Zhang", url=os.getenv("URL"))
 
 @fellow_nav.route('/helen')
 def helen():
