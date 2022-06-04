c545a78be77a1572cee58cf3e6378d25b8218a42
IntentusKristen <85136882+IntentusKristen@users.noreply.github.com>
2022-06-04 13:37:31 -0400

    adding jinja template for experiences

--- app/fellow_nav.py
+++ app/fellow_nav.py
@@ -7,7 +7,9 @@ fellow_nav = Blueprint('fellow_nav', __name__)
 
 @fellow_nav.route('/kristen')
 def kristen():
-    return render_template('kristen_page.html', title="22.SUM.22 Fellow: Kristen Zhang", url=os.getenv("URL"))
+    return render_template('kristen_page.html', title="22.SUM.22 Fellow: Kristen Zhang", url=os.getenv("URL"), 
+    title_location="Summer Research Intern @ Western University", date="May 2022 - Present",
+    title_location2="Production Engineering Fellow @ MLH", date2 = "May 2022 - Present")
 
 @fellow_nav.route('/helen')
 def helen():
