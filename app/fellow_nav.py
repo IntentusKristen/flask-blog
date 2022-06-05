178623ea0ec726df19d960f82504de5541cc1705
Helen Xia <y95xia@uwaterloo.ca>
2022-06-05 15:16:38 -0400

    fixed template format and started helen html

--- app/fellow_nav.py
+++ app/fellow_nav.py
@@ -5,6 +5,12 @@ import os
 # kind of separate our app out, don't have to have all our views defined in one file, can have them defined in multiple files
 fellow_nav = Blueprint('fellow_nav', __name__)
 
+# nav
+nav = [{'name': 'Home', 'url': '/'},
+           {'name': 'Kristen', 'url': '/kristen'},
+           {'name': 'Helen', 'url': '/helen'},
+           {'name': 'Catherine', 'url': '/catherine'}]
+
 @fellow_nav.route('/kristen')
 def kristen():
     work_info =  [{
@@ -39,7 +45,41 @@ def kristen():
 
 @fellow_nav.route('/helen')
 def helen():
-    return "<p>Helen Xia<p>"
+    work_info =  [{
+            'name': 'Business Anaylst',
+            'location': 'Scotiabank',
+            'start_date': 'May 2022',
+            'end_date': 'Present',
+            'desc': 'Works with key stakeholders to align technology solutions with business strategies'
+        },{
+            'name': 'Production Engineering Fellow',
+            'location': 'MLH',
+            'start_date': 'May 2022',
+            'end_date': 'Present',
+            'desc': 'Production Engineering Fellowship hosted by MLH and Meta'
+        },{
+            'name': 'Software Engineer',
+            'location': 'Scotiabank',
+            'start_date': 'Sept 2021',
+            'end_date': 'Dec 2021',
+            'desc': 'Created regression models using Python and SQL to help price International ETFs'
+        }]
+
+    education_info = [{
+            'name': 'University of Waterloo',
+            'location': 'Waterloo, ON',
+            'start_date': 'September 2020',
+            'end_date': 'Present',
+            'desc' : 'Bachelor of Computer Science Co-op'
+        },{
+            'name': 'Iroquois Ridge High School',
+            'location': 'Oakville, ON',
+            'start_date': 'September 2016',
+            'end_date': 'June 2020',
+            'desc' : 'Debate Club Marketing Executive, Interact Club Marketing Executive'
+        }]
+
+    return render_template('helen_page.html', nav=nav, title="Helen Xia", url=os.getenv("URL"), work_info=work_info, education_info=education_info)
 
 @fellow_nav.route('/catherine')
 def catherine():
