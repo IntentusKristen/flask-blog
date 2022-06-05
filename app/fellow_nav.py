c12cc471cba536228c238d3f2e6174e7917f1194
cjlaserna <49669196+cjlaserna@users.noreply.github.com>
2022-06-04 19:30:30 -0700

    added work experiences based on new jinja template

--- app/fellow_nav.py
+++ app/fellow_nav.py
@@ -43,8 +43,34 @@ def helen():
 
 @fellow_nav.route('/catherine')
 def catherine():
+    work_info =  [{
+            'name': 'Production Engineer Fellow',
+            'location': 'MLH',
+            'start_date': ' ',
+            'end_date': ' ',
+            'desc': "The MLH Fellowship is a 12-week internship alternative for aspiring technologists. Fellows on the Production Engineering track experience what it's like to work in Production / Site Reliability Engineering and DevOps."
+        },{
+            'name': 'Intern',
+            'location': '1,000 Dreams Fund',
+            'start_date': ' ',
+            'end_date': ' ',
+            'desc': 'I contribute through both research for outreach and contact building, as well as well as match, pair, and facilitate MentorHER mentorship sessions. '
+        },{
+            'name': 'Student Advisory Board ',
+            'location': '1,000 Dreams Fund',
+            'start_date': ' ',
+            'end_date': ' ',
+            'desc': "I contributed through marketing and was apart of a board that facilitated the scholarship winners for 1000 Dream Fund's student scholarships.  "
+        },{
+            'name': 'Summer Intern',
+            'location': 'Accenture',
+            'start_date': ' ',
+            'end_date': ' ',
+            'desc': "I completed a 6-week survey course focused on introducing the professional culture of global business and topics, skills, and issues that are defining the future of work. I collaborated with 7 interns to create a group project which included a final presentation for Accenture and non-profit guests."
+        }]
+
     nav = [{'name': 'Home', 'url': '/'},
            {'name': 'Kristen', 'url': '/kristen'},
            {'name': 'Helen', 'url': '/helen'},
            {'name': 'Catherine', 'url': '/catherine'}]
-    return render_template('catherine_page.html', nav=nav, title="22.SUM.22 Fellow: Catherine Laserna", url=os.getenv("URL"))
+    return render_template('catherine_page.html', nav=nav, work_info=work_info, title="22.SUM.22 Fellow: Catherine Laserna", url=os.getenv("URL"))
