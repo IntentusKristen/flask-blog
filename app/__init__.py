ae4380dafc8aa7a66c7f605888cfa6d319c9bd80
IntentusKristen <85136882+IntentusKristen@users.noreply.github.com>
2022-06-06 01:28:25 -0400

    made second link

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 from .fellow_nav import fellow_nav
+from .k_hobbies import k_hobbies
+
 app.register_blueprint(fellow_nav, url_prefix='/')
+app.register_blueprint(k_hobbies, url_prefix='/kristen/')
 
 
 @app.route('/')
@@ -16,3 +19,7 @@ def home():
            {'name': 'Helen', 'url': '/helen'},
            {'name': 'Catherine', 'url': '/catherine'}]
     return render_template('home.html', nav=nav, title="MLH Fellow Orientation Hack", url=os.getenv("URL"))
+
+@app.route('/kristen/hobbies')
+def kristen_hobbies():
+    return render_template('kristen_hobbies.html', title="My Hobbies", url=os.getenv("URL"))
---
+++ app/k_hobbies.py
@@ -0,0 +1,5 @@
+from flask import Blueprint, render_template
+import os
+
+k_hobbies = Blueprint('k_hobbies', __name__)
+
