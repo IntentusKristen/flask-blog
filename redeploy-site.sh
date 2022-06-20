89c0c3d39e45564dffe27c88f8685ceaaad4323a
IntentusKristen <85136882+IntentusKristen@users.noreply.github.com>
2022-06-20 00:59:12 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+tmux kill-server
+
+cd flask-blog
+
+git fetch && git reset origin/main --hard
+
+python -m venv python3-virtualenv
+
+pip install requirements.txt
+
+tmux new -s redeployed_sess
+
+:attach-session -t . -c flask-blog
+
+tmux detach
+
+source python3-virtualenv/bin/activate
+
+flask run
