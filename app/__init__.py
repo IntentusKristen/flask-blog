8b269ed325eea07ef32b0af6ca0c47438301a4c5
IntentusKristen <85136882+IntentusKristen@users.noreply.github.com>
2022-06-23 13:04:08 -0400

    sql

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 import os
 from flask import Flask, render_template, make_response
 from dotenv import load_dotenv
+from peewee import *
 
 load_dotenv()
 app = Flask(__name__)
 from .fellow_nav import fellow_nav
+
+mydb = MYSQLDatabase(os.getenv("MYSQL_DATABASE"),
+       user=os.getenv("MYSQL_USER"),
+       password=os.getenv("MYSQL_PASSWORD"),
+       host=os.getenv("MYSQL_HOST"),
+       port=3306)
+
+print(mydb)
 
 app.register_blueprint(fellow_nav, url_prefix='/')
 
