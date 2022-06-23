002874b376775bacb70037ea60acc384b6a3d5d2
IntentusKristen <85136882+IntentusKristen@users.noreply.github.com>
2022-06-23 16:41:39 -0400

    curl

--- app/__init__.py
+++ app/__init__.py
@@ -87,3 +87,10 @@ def get_time_line_post():
                      for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
               ]
        }
+
+@app.route('/api/timeline_post', methods=['DELETE'])
+def delete_time_line_post():
+       id = request.form['id']
+
+       TimelinePost.delete_by_id(id)
+          
---
+++ curl-test.sh
@@ -0,0 +1,6 @@
+# creating random timeline post
+curl -X POST http://localhost:5000/api/timeline_post -d 'name=Kristen&email=intentuskristen@gmail.com
+&content=Testing endpoints with postman and curl!'
+
+# getting the endpoint
+curl http://localhost:5000/api/timeline_post
